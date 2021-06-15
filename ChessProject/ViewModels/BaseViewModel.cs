30fa9b300df3b48d245e349c754552633eb6c3a6
Alex Seres <lilaalex95@gmail.com>
2021-06-15 11:10:22 +0100

    created baseviewmodel which will be the base class of all viewmodel and implemented onpropertychanged method

---
+++ ChessProject/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ChessProject.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler changed = PropertyChanged;
+            if (changed == null) return;
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
