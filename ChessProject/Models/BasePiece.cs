ac0de9a071b85106d1431ffaf1fc4383e3bdce24
Alex Seres <lilaalex95@gmail.com>
2021-06-15 12:03:56 +0100

    created basepiece of models which is the based of every chess piece

---
+++ ChessProject/Models/BasePiece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChessProject.Models
+{
+    public abstract class BasePiece : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name { get; set; }
+
+        
+
+    }
+}
--- ChessProject/ViewModels/BaseViewModel.cs
+++ ChessProject/ViewModels/BaseViewModel.cs
@@ -11,8 +11,15 @@ namespace ChessProject.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
 
+        public bool SetProperty<T>(ref T backStore, T newValue,[CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backStore, newValue)) return false;
+            backStore = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
 
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler changed = PropertyChanged;
             if (changed == null) return;
