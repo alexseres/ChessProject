bd74153d5d385f18a4726f5f6ef2f1184aacb253
Alex Seres <lilaalex95@gmail.com>
2021-06-15 10:51:43 +0100

    created relaycommand: added some fields and implemented constructor

---
+++ ChessProject/Commands/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ChessProject.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        public Action<T> _execute;
+        public Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
