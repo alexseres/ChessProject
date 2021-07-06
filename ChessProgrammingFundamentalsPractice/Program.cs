91548fad2798260031684be339e7556a63755826
Alex Seres <lilaalex95@gmail.com>
2021-07-06 12:27:03 +0100

    created string board which shows the pices with different character

--- ChessProgrammingFundamentalsPractice/Program.cs
+++ ChessProgrammingFundamentalsPractice/Program.cs
@@ -4,7 +4,8 @@ namespace ChessProgrammingFundamentalsPractice
 {
     public class Program
     {
-        static void Main(string[] args)
+
+        public static void JustTestGeneralChessThings()
         {
             //BitwiseOperatorsGeneral.CheckBitwiseComplementOperators();
             //BitwiseOperatorsGeneral.CheckLeftShiftOperator();
@@ -17,7 +18,7 @@ namespace ChessProgrammingFundamentalsPractice
             //BitwiseOperatorsGeneral.RotateRight(129,2);
 
             ulong bitboard = 0b_0000_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_1111_1111;
-            ulong bitboard2= 0b_0000_0000_0000_0000_1110_1111_0000_0000_0110_0000_1110_0000_0000_0000_0000_0000;
+            ulong bitboard2 = 0b_0000_0000_0000_0000_1110_1111_0000_0000_0110_0000_1110_0000_0000_0000_0000_0000;
             ulong bitboard3 = 0b_0000_0000_1100_0000_0010_0010_0000_0000_0000_0000_0000_0000_0000_0000_0011_0000;
             ulong bitboard4 = 0b_0000_0000_0000_0110_0000_0111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
             //BitwiseOperatorsGeneral.GeneralizedShit(bitboard, 9);
@@ -33,7 +34,13 @@ namespace ChessProgrammingFundamentalsPractice
             //BitwiseOperatorsGeneral.flipVertical(bitboard3);
             //BitwiseOperatorsGeneral.flipHorizontal(bitboard3);
             //BitwiseOperatorsGeneral.FlipDiagonal(bitboard4);       
-            BitwiseOperatorsGeneral.FlipAntiDiagonal(bitboard4); 
+            BitwiseOperatorsGeneral.FlipAntiDiagonal(bitboard4);
+        }
+        static void Main(string[] args)
+        {
+            ProofOfConcept proofOfConcept = new ProofOfConcept();
+            var board = proofOfConcept.CreateStringOfBoard();
+            proofOfConcept.PrintBoard(board);
         }
     }
 }
--- ChessProgrammingFundamentalsPractice/ProofOfConcept.cs
+++ ChessProgrammingFundamentalsPractice/ProofOfConcept.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChessProgrammingFundamentalsPractice
@@ -8,18 +9,103 @@ namespace ChessProgrammingFundamentalsPractice
     {
         public ulong BoardWithAllMember = 0b_1111_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_1111_1111;
         public ulong WhitePieces = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_1111_1111;
-        public ulong BlackPieces = 0b_1111_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-        public ulong WhiteRooks = 0b_1000_0001_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-        public ulong BlackRooks = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1000_0001;
+        public ulong WhiteRooks = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1000_0001;
         public ulong WhiteKnights = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0100_0010;
-        public ulong BlackKnights = 0b_0010_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
         public ulong WhiteBishops = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0010_0100;
-        public ulong BlackBishops = 0b_0100_0010_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
         public ulong WhiteQueen = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1000;
-        public ulong BlackQueen = 0b_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
-        public ulong WhiteKing = 0b_0010_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1000_0000;
-        public ulong BlackKing = 0b_0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong WhiteKing = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0001_0000;
         public ulong WhitePawns = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111_0000_0000;
+        public ulong BlackPieces = 0b_1111_1111_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackRooks = 0b_0001_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackKnights = 0b_0010_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackBishops = 0b_0100_0010_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackQueen = 0b_0001_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackKing = 0b_0000_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+        public ulong BlackPawns = 0b_0000_0000_1111_1111_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
 
+
+        public List<(string, ulong)> AddLetterIndicatorToPieces = new List<(string, ulong)>();
+
+        public ProofOfConcept()
+        {
+        }
+
+        public void PrintBoard(string board)
+        {
+
+            
+        }
+
+        public string CreateStringOfBoard()
+        {
+            var mask = 0b_1000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000;
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0;i< 64;i++)
+            {   
+                if((BlackRooks & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("R");
+                }
+                else if ((BlackKnights & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("K");
+                }
+                else if ((BlackBishops & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("B");
+                }
+                else if ((BlackQueen & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("Q");
+                }
+                else if ((BlackKing & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("N");
+                }
+                else if ((BlackPawns & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("P");
+                }
+                else if ((WhiteRooks & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("A");
+                }
+                else if ((WhiteKnights & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("S");
+                }
+                else if ((WhiteBishops & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("D");
+                }
+                else if ((WhiteQueen & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("F");
+                }
+                else if ((WhiteKing & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("G");
+                }
+                else if ((WhitePawns & (mask & BoardWithAllMember)) > 0)
+                {
+                    sb.Append("H");
+                }
+
+                else
+                {
+                    sb.Append(".");
+                }
+
+
+                mask = mask >> 1;
+            }
+            Console.WriteLine(sb.ToString());
+            return sb.ToString();
+        }
     }
 }
+
+                //Console.WriteLine(tupl.Item1);
+                //Console.WriteLine(Convert.ToString((long)BoardWithAllMember, toBase: 2).PadLeft(64, '0'));
+                //Console.WriteLine(Convert.ToString((long)tupl.Item2, toBase: 2).PadLeft(64,'0'));
+                //Console.WriteLine(Convert.ToString((long)(tupl.Item2 & BoardWithAllMember), toBase:2).PadLeft(64, '0')); 
