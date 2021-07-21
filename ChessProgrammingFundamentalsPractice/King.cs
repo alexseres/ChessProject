42ce9281d21fc9dcd52ce22e7cbb1daa274cf2d0
Alex Seres <lilaalex95@gmail.com>
2021-07-21 18:40:51 +0100

    king search also being done

--- ChessProgrammingFundamentalsPractice/King.cs
+++ ChessProgrammingFundamentalsPractice/King.cs
@@ -6,15 +6,36 @@ namespace ChessProgrammingFundamentalsPractice
 {
     public class King : BasePiece
     {
+        private ulong MaskNotInAFile = 0b_0111_1111_0111_0111_1111_0111_1111_1111_1111_0111_1111_0111_1111_0111_1111_0111;
+        private ulong MaskNotInHFile = 0b_1111_1110_1111_1110_1111_1110_1111_1110_1111_1110_1111_1110_1111_1110_1111_1110;
+
+        private const int NorthDirection = 8;
+        private const int EastDiretion = -1;
+        private const int SouthDirection = -8;
+        private const int WestDirection = 1;
+        private const int EastNorthDirection = 7;
+        private const int WestNorthDirection = 9;
+        private const int EastSouthDirection = -9;
+        private const int WestSouthDirection = -7;
 
         public King(ColorSide color, ulong positions): base(color, positions)
         {
-
+            
         }
 
         public override ulong Search(ulong currentPosition, ulong allPositionAtBoard, ulong opponentPositionAtBoard, ulong ourPositions)
         {
-            throw new NotImplementedException();
+            ulong northAttack = currentPosition << 8;
+            ulong northEastAttack = (currentPosition & MaskNotInHFile) << 7;
+            ulong northWestAttack = (currentPosition & MaskNotInAFile) << 9;
+            ulong westAttack = (currentPosition & MaskNotInAFile) << 1;
+            ulong eastAttack = (currentPosition & MaskNotInHFile) >> 1;
+            ulong southEastAttack = (currentPosition & MaskNotInHFile) >> 9;
+            ulong southWestAttack =(currentPosition & MaskNotInAFile) >> 7;
+            ulong southAttack = currentPosition >> 8;
+            ulong allAttacks = northAttack ^ northEastAttack ^ northWestAttack ^ westAttack ^ eastAttack ^ southEastAttack ^ southWestAttack ^ southAttack;
+            ulong allPossibilities = (allAttacks & ~ourPositions);
+            return allPossibilities;
         }
     }
 }
